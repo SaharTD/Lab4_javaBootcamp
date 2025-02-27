3d24b7f47f7a372d43a1bcbab0c20ef853fc1acd
Sahar Turki <103968284+SaharTD@users.noreply.github.com>
2025-02-26 23:30:25 -0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,25 @@ public class Main {
     public static void main(String[] args) {
 
 
-      Circle c1=new Circle(4.5);
+      Circle c1=new Circle(4.6);
       Rectangle r1 =new Rectangle(8,9);
       Triangle t1 = new Triangle(18,30);
 
+      //the result rounded to the nearest int
+
         System.out.println("The radius of the circle is =  "+c1.getRaduis());
-        System.out.println("The area of the circle is = "+c1.calculateArea()+ "\nthe circumference of the circle is = "+c1.calculateCircumference());
+        System.out.println("The area of the circle is = "+Math.round(c1.calculateArea())+ "\nthe circumference of the circle is = "+Math.round(c1.calculateCircumference()));
 
         System.out.println();
 
-        System.out.println("The height of the rectangle is =  "+r1.getHeight()+"\nthe width of the rectangle is = "+r1.getWidth());
-        System.out.println("The area of the rectangle is = "+r1.calculateArea()+ "\nthe circumference of the rectangle is = "+r1.calculateCircumference());
+        System.out.println("The height of the rectangle is =  "+r1.getHeight()+"\n the width of the rectangle is = "+r1.getWidth());
+        System.out.println("The area of the rectangle is = "+Math.round(r1.calculateArea())+ "\nthe circumference of the rectangle is = "+Math.round(r1.calculateCircumference()));
 
         System.out.println();
 
 
         System.out.println("The base of the triangle is =  "+t1.getBase() + "\nThe height of the triangle is ="+t1.getHeight());
-        System.out.println("The area of the triangle is = "+t1.calculateArea()+ "\nthe circumference of the triangle is = "+t1.calculateCircumference());
+        System.out.println("The area of the triangle is = "+Math.round(t1.calculateArea())+ "\nthe circumference of the triangle is = "+Math.round(t1.calculateCircumference()));
 
 
 
